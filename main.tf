1430f602cb8a94ab994ce7ecba71d7ca067b3f70
AzureAD\MislavJelusic <mislav.jelusic@syntio.net>
2021-12-08 09:13:48 +0100

    add files

---
+++ main.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = var.gcp_auth_file
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags         = ["web", "dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "test-firewall"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "udp"
+  }
+  allow {
+    protocol = "sctp"
+  }
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8080", "1000-2000","22"]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "project" { }
+
+variable "region" {
+  default = "europe-west4"
+}
+
+variable "zone" {
+  default = "europe-west4-a"
+}
+
+variable "gcp_auth_file" {
+}
